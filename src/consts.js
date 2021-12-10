aae1b2f84d3c73f1f5ea776dfbf61beaef6053a8
aneeta-f <aneeta.ochani@hotmail.com>
2021-12-09 23:35:08 -0500

    Frontend: Update host

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,6 @@
-const API_HOST = 'http://localhost:4000';
+//const API_HOST = 'http://localhost:4000';
+const API_HOST = 'https://node-backend-portal.herokuapp.com';
+
 const API_URL = `${API_HOST}/api`;
 
 const API_LOGIN = `${API_HOST}/api/login`;
